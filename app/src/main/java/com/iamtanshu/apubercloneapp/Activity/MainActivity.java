2e3214ebab8455aff7dcf20520709df68841ad40
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2020-04-12 12:51:26 +0530

    Application class

--- app/src/main/java/com/iamtanshu/apubercloneapp/Activity/MainActivity.java
+++ app/src/main/java/com/iamtanshu/apubercloneapp/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.iamtanshu.apubercloneapp;
+package com.iamtanshu.apubercloneapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.iamtanshu.apubercloneapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
