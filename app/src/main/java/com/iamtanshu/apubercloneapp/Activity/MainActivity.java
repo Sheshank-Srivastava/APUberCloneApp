9b31ca02b7f761283b8cc9b3adb53bc63291d078
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2020-04-12 14:37:34 +0530

    login page is done

--- app/src/main/java/com/iamtanshu/apubercloneapp/Activity/MainActivity.java
+++ app/src/main/java/com/iamtanshu/apubercloneapp/Activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.iamtanshu.apubercloneapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.iamtanshu.apubercloneapp.R;
 import com.parse.Parse;
@@ -16,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.signup_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sign_up:
+                Toast.makeText(this, "Sign Up is Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true ;
     }
 }
